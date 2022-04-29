8802402e35685efcd76bd69663dc99f2b1c354d2
深蓝 <mail@ri-co.cn>
2022-04-29 09:27:37 +0800

    添加项目文件。

---
+++ distiot-c-sdk.cpp
@@ -0,0 +1,13 @@
+﻿// distiot-c-sdk.cpp: 定义应用程序的入口点。
+//
+
+#include "distiot-c-sdk.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	hello2();
+	return 0;
+}
---
+++ distiot-c-sdk.h
@@ -0,0 +1,22 @@
+﻿// distiot-c-sdk.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <stdio.h>
+
+struct DeviceManager
+{
+	char token[37];
+	char masterUrl[100];
+	char userUrl[100];
+};
+
+struct Device
+{
+	unsigned int id;
+	char token[37];
+	char nodeAddr[100];
+	int nodePort;
+};
+
